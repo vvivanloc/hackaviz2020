782e11539c6ba06fee91c8f619ab918c25630d94
nop <nop@nop>
2019-03-23 20:19:51 +0100

    feat (map) marker, info from towns

--- src/leaf-demo.ts
+++ src/leaf-demo.ts
@@ -1,22 +1,32 @@
 import L from 'leaflet';
 import markers from '../maps/markers.json';
 
-
 declare const par_commune: any;
+
+import { Commune } from './model/commune';
+
+const features = {
+  showCommuneDelimitations: false,
+  showCommuneCenters: true
+};
+
+const nbMaxCommunes: number = 100;
 
 function main() {
   var map = L.map('map', {
     center: [43.5, 0.5],
-    minZoom: 5,
+    minZoom: 2,
+    // minZoom: 5,
     zoom: 7
-  })
+  });
 
   L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+    attribution:
+      '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>',
     subdomains: ['a', 'b', 'c']
-  }).addTo(map)
+  }).addTo(map);
 
-  let myURL = 'maps/'
+  let myURL = 'maps/';
 
   var myIcon = L.icon({
     iconUrl: myURL + 'images/pin24.png',
@@ -24,18 +34,59 @@ function main() {
     iconSize: [29, 24],
     iconAnchor: [9, 21],
     popupAnchor: [0, -14]
-  })
+  });
 
   for (var i = 0; i < markers.length; ++i) {
     L.marker([markers[i].lat, markers[i].lng], { icon: myIcon })
-      .bindPopup('<a href="' + markers[i].url + '" target="_blank">' + markers[i].name + '</a>')
+      .bindPopup(
+        '<a href="' +
+          markers[i].url +
+          '" target="_blank">' +
+          markers[i].name +
+          '</a>'
+      )
       .addTo(map);
   }
 
-  /* Hydro */
-  const hydro = new L.LayerGroup();
-  L.geoJSON((par_commune as any)).addTo(hydro);
-  hydro.addTo(map);
+  if (features.showCommuneDelimitations) {
+    const communes = new L.LayerGroup();
+    L.geoJSON(par_commune as any).addTo(communes);
+    communes.addTo(map);
+  }
+
+  if (features.showCommuneCenters) {
+    let communes = par_commune.features as Array<any>;
+    if (nbMaxCommunes !== -1) {
+      communes = communes.slice(0, nbMaxCommunes);
+    }
+
+    const infoDomElement = document.getElementById('info');
+    function markerOnClick(event: Event) {
+      var props = event.target['properties'];
+      console.log(props);
+      if (infoDomElement) {
+        infoDomElement.innerHTML = props.join('<br>');
+      }
+    }
+    communes.forEach(commune => {
+      const communeProps = commune.properties as Commune;
+
+      const props = JSON.stringify(communeProps).split(',');
+
+      const marker = L.marker([communeProps.longitude, communeProps.latitude], {
+        icon: myIcon
+      })
+        .bindPopup(
+          ` <b>${communeProps.commune}</b><br>${props
+            .slice(0, 10)
+            .join('<br>')}`
+        )
+        .on('click', markerOnClick)
+        .addTo(map);
+
+      marker['properties'] = props;
+    });
+  }
 }
 
 main();
--- webpack.config.js
+++ webpack.config.js
@@ -1,37 +1,41 @@
 const path = require('path');
 const CopyPlugin = require('copy-webpack-plugin');
 
-
 module.exports = (env, argv) => {
-	return {
-		context: path.resolve(__dirname, './src'),
+  return {
+    context: path.resolve(__dirname, './'),
 
-		entry: {
-			app: './leaf-demo.ts'
-		},
+    entry: {
+      app: './src/leaf-demo.ts'
+    },
 
-		output: {
-			filename: 'bundle.js',
-			path: path.resolve(__dirname, './')
-		},
+    output: {
+      filename: 'bundle.js',
+      path: path.resolve(__dirname, './')
+    },
 
-		resolve: {
-			extensions: ['.ts', '.js']
-		},
+    resolve: {
+      extensions: ['.ts', '.js']
+    },
 
-		devtool: 'source-map',
+    devtool: 'source-map',
 
-		module: {
-			rules: [
-				{ test: /\.tsx?$/, loader: 'ts-loader' },
-				
-			]
-		},
-		plugins: [
-			new CopyPlugin([
-			  { from: '../node_modules/leaflet/dist/leaflet.css', to: 'leaflet' },
-			]),
-		  ],
-
-	}
+    module: {
+      rules: [
+        {
+          test: /\.tsx?$/,
+          loader: 'ts-loader'
+        },
+        {
+          test: /\.less$/,
+          loader: 'less-loader'
+        }
+      ]
+    },
+    plugins: [
+      new CopyPlugin([
+        { from: './node_modules/leaflet/dist/leaflet.css', to: 'leaflet' }
+      ])
+    ]
+  };
 };
